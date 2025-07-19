46ff0426494b64cbfe45f282e47fef4b127a27a8
JEWEL  J JOSEPH <jeweljoseph807@gmail.com>
2025-07-19 20:33:46 +0530

    Update PrimeNum.java

--- src/PrimeNum.java
+++ src/PrimeNum.java
@@ -3,6 +3,7 @@ public class PrimeNum {
     public static void main(String[] args) {
         System.out.println("Prime numbers between 1 and 100 are:"); 
         for (int i = 2; i <= 100; i++) {
+            boolean isPrime = true;
                 if (i % j == 0) {
                     isPrime = false;
                     break;
