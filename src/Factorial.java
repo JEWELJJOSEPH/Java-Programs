87e5cbb8ee25777ec5973ed8fa6eccc5d2b3bd11
jeweljjoseph907 <jeweljjoseph2028@cs.sjcetpalai.ac.in>
2025-07-19 14:58:55 +0530

    Second-Commit

---
+++ src/Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args){
+    int i = 1;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number:");
+    int num = sc.nextInt();
+    long fact = 1;
+    while(i <= num){
+    fact *= i;
+    i++;
+    }
+    System.out.printf("factorail of the %d = %d%n",num,fact);
+    
+}
+}
--- src/even.java
+++ src/even.java
@@ -2,9 +2,9 @@ public class even {
     public static void main(String[] args) throws Exception {
        int i = 0;
        System.out.println("Even Numbers from 1-20 are:");
-       while (i <=20) {
+       do{
+        i += 2;
         System.out.println(i);
-        i+= 2;
-       }
+    }while (i < 20);   
     }
 }
