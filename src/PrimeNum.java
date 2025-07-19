d482b2646ed9973633c20ccadc7417c1a98cae7a
JEWEL  J JOSEPH <jeweljoseph807@gmail.com>
2025-07-19 16:20:22 +0530

    Create PrimeNum.java

---
+++ src/PrimeNum.java
@@ -0,0 +1,15 @@
+//Jewel J Joseph 19/07/25
+public class PrimeNum {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 100 are:"); 
+        for (int i = 2; i <= 100; i++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }if (isPrime) {
+                System.out.print(i + " "); // Print the prime number
+            }
+        }
+    }
+}
