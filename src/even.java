7c573dc6f434c22fd5ea88eb9183e2f540be5cd0
jeweljjoseph907 <jeweljjoseph2028@cs.sjcetpalai.ac.in>
2025-07-19 13:13:55 +0530

    first commit

---
+++ src/even.java
@@ -0,0 +1,10 @@
+public class even {
+    public static void main(String[] args) throws Exception {
+       int i = 0;
+       System.out.println("Even Numbers from 1-20 are:");
+       while (i <=20) {
+        System.out.println(i);
+        i+= 2;
+       }
+    }
+}
